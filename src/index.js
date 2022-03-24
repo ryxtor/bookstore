157480019c807be39bc74a0fcb4505278b0d0ed3
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-03-24 14:52:06 -0300

    Add Header and routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { HashRouter } from 'react-router-dom';
+import Header from './components/Header';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Header />
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
