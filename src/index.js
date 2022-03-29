b95727f04af879c11af3deaea0db4ad987a0e4be
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-03-29 17:57:23 -0300

    Add Provider and store in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
+import store from './redux/configureStore';
 import Header from './components/Header';
 import App from './App';
 
@@ -9,7 +11,9 @@ ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
       <Header />
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </HashRouter>
   </React.StrictMode>,
   document.getElementById('root'),
